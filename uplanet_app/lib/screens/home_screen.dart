67a420a9133137fd011cacdc6a49ebefaaaa62cd
Fro Warner <froennkastle@gmail.com>
2019-11-21 22:56:16 -0800

    Screen Layout added
    +HomeScreen
    +LoginScreen
    +SettingsScreen

---
+++ uplanet_app/lib/screens/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        //Earth Element/Animation
+        //Acc/Settings Drawer Element
+        //Notification Element
+        //Camera Access Element
+        //Hello Friend Animation
+        //
+      ),
+    );
+  }
+}
+
+// Earth element enlarged view
+
+
+// Calendar element enlarged view
+
+
+// Statistics element enlarged view
